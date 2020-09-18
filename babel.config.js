9d443621cdd41876834cbc361e1a1870c5070987
zhengxiaoxiao <18255068953@163.com>
2020-09-18 13:53:07 +0800

    feat: vue 配置(生产环境去掉所有console.log + 打包压缩）

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+/*
+ * @Author: zhengxiaoxiao
+ * @Date: 2020-09-15 14:21:09
+ * @Description: 
+ */
+// 生产环境下，去掉所有的console.log
+const prodPlugin = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugin.push([
+    'transform-remove-console',
+    {
+      exclude: ['error', 'warn']
+    }
+  ])
+}
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    prodPlugin
+  ]
 };
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: zhengxiaoxiao
+ * @Date: 2020-09-18 13:18:59
+ * @Description: 启用压缩，让页面加载更快
+ */
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const isProd = process.env.NODE_ENV === 'production'
+module.exports = {
+  configureWebpack: config => {
+    if (isProd) {
+      // 配置webpack 压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css$/,
+          // 超过4kb压缩
+          threshold: 4096
+        })
+      )
+    }
+  }
+}
