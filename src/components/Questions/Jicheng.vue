a52c19be52679b3f1df5569f9a234228d3d7bf50
郑肖肖 <zhengxiaoxiao@mlogcn.com>
2021-09-26 18:18:54 +0800

    feat  es6 继承

---
+++ src/components/Questions/Jicheng.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Description: es6 继承
+ * @Autor: zxx
+ * @Date: 2021-09-18 11:12:04
+ * @LastEditors: zxx
+ * @LastEditTime: 2021-09-18 11:44:02
+-->
+<template>
+  <div>{{ 'es6 继承' }}</div>
+</template>
+<script>
+import Children1 from '../../common/constant'
+export default {
+  name: 'Jicheng',
+  data() {
+    return {}
+  },
+  mounted() {
+    var child = new Children1('red')
+    console.log(child.toString(), 'jicheng')
+  },
+}
+</script>
