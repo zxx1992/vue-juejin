8affc60f277b04e058134d73390e6109394708c7
郑肖肖 <zhengxiaoxiao@mlogcn.com>
2021-10-11 11:19:50 +0800

    fix  动态软件   结合 component  is  computed  一起使用

---
+++ src/components/Questions/tabposts.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Description: ...页面文件
+ * @Autor: zxx
+ * @Date: 2021-10-11 10:27:15
+ * @LastEditors: zxx
+ * @LastEditTime: 2021-10-11 10:30:22
+-->
+<template>
+  <div class="home">
+    tabposts
+  </div>
+</template>
+<script>
+export default {
+  name: 'tabposts',
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,15 +3,36 @@
  * @Autor: zxx
  * @Date: 2020-12-01 22:20:41
  * @LastEditors: zxx
- * @LastEditTime: 2021-09-26 20:54:30
+ * @LastEditTime: 2021-10-11 10:56:35
 -->
 <template>
   <div class="about">
     <Button @click="onChange">Change login status</Button>
+
+    <Button v-for="tab in tabs" :key="tab" @click="currentTab = tab">{{ tab }}</Button>
+    <component :is="currentTabComponent"></component>
   </div>
 </template>
 <script>
+import tabhome from '../components/Questions/tabhome.vue'
+import tabposts from '../components/Questions/tabposts.vue'
 export default {
+  mame: 'About',
+  components: {
+    tabhome,
+    tabposts,
+  },
+  data() {
+    return {
+      currentTab: 'home',
+      tabs: ['home', 'posts', 'archive'],
+    }
+  },
+  computed: {
+    currentTabComponent() {
+      return 'tab' + this.currentTab
+    },
+  },
   methods: {
     onChange() {
       this.$store.commit('setLoginstatus', { loginstatus: true })
